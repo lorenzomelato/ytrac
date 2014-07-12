4260194e5e0b95ac4ebc538ee962459e1681f457
Lorenzo Melato <lorenzo.melato@ynnovahq.com>
2014-07-12 21:14:07 +0200

    Added the ASP.NET MVC project skeleton.

---
+++ Ynnova.YTrac.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ynnova.YTrac.Web.MvcApplication" Language="C#" %>
---
+++ Ynnova.YTrac.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ynnova.YTrac.Web.Startup))]
+namespace Ynnova.YTrac.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
