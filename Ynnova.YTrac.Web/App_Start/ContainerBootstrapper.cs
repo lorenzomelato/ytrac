cd4941e52e2827dfca12c284d8afb866d2a32058
Lorenzo Melato <lorenzo.melato@ynnovahq.com>
2014-07-12 23:39:30 +0200

    Minor.

--- Ynnova.YTrac.Web/App_Start/ContainerBootstrapper.cs
+++ Ynnova.YTrac.Web/App_Start/ContainerBootstrapper.cs
@@ -4,30 +4,30 @@ using Castle.Windsor.Installer;
 
 namespace Ynnova.YTrac.Web.App_Start
 {
-    public class ContainerBootstrapper : IContainerAccessor, IDisposable
-    {
-        readonly IWindsorContainer container;
+	public class ContainerBootstrapper : IContainerAccessor, IDisposable
+	{
+		readonly IWindsorContainer container;
 
-        ContainerBootstrapper(IWindsorContainer container)
-        {
-            this.container = container;
-        }
+		ContainerBootstrapper(IWindsorContainer container)
+		{
+			this.container = container;
+		}
 
-        public IWindsorContainer Container
-        {
-            get { return container; }
-        }
+		public IWindsorContainer Container
+		{
+			get { return container; }
+		}
 
-        public static ContainerBootstrapper Bootstrap()
-        {
-            var container = new WindsorContainer().
-                Install(FromAssembly.This());
-            return new ContainerBootstrapper(container);
-        }
+		public static ContainerBootstrapper Bootstrap()
+		{
+			var container = new WindsorContainer().
+				Install(FromAssembly.This());
+			return new ContainerBootstrapper(container);
+		}
 
-        public void Dispose()
-        {
-            Container.Dispose();
-        }
-    }
+		public void Dispose()
+		{
+			Container.Dispose();
+		}
+	}
 }
